a5a2db8775fe1886b4d4e3fd55af7d5776bf30f8
tomoki <tmdesign.0223@gmail.com>
2025-12-29 15:49:24 +0900

    一覧ページ作成

---
+++ resources/views/livewire/post-index.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <flux:heading size="xl" lebel="1">投稿一覧ページ</flux:heading>
+
+    <div class="mt-4">
+        <flux:button href='#' variant="primary" color="blue">新規投稿</flux:button>
+    </div>
+</div>
