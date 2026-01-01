f0cafe3eb8a1d31c46fc3d75e9fa48819ce34630
tomoki <tmdesign.0223@gmail.com>
2026-01-02 03:24:55 +0900

    細かい修正

--- resources/views/livewire/update-post.blade.php
+++ resources/views/livewire/update-post.blade.php
@@ -2,14 +2,12 @@
     <flux:heading size="xl" lebel="1">投稿編集ページ</flux:heading>
 
     <div class="w-3xl m-auto">
-        @if (session('status'))
-            <span class="text-blue-400 bg-white p-1 w-full block rounded-md">{{ session('status') }}</span>
-        @endif
+
         {{-- フォーム --}}
         <form wire:submit="update">
             <div class="w-3xl mt-4">
-                <input type="text" wire:model="title" placeholder="タイトルを入力してください" class="w-full border-1 rounded-md p-2"
-                    value="{{ $post->title }}">
+                <input type="text" wire:model="title" placeholder="タイトルを入力してください"
+                    class="w-full border-1 rounded-md p-2" value="{{ $post->title }}">
                 @error('title')
                     <span class="text-red-400">{{ $message }}</span>
                 @enderror
@@ -21,7 +19,8 @@
                 @enderror
             </div>
             <div class="flex justify-end mt-4 gap-2">
-                <flux:button href="{{ route('posts') }}" variant="primary" color="blue">一覧に戻る</flux:button>
+                <flux:button href="{{ route('posts.show', $post->id) }}" variant="primary" color="blue">詳細に戻る
+                </flux:button>
                 <div>
                     <flux:button type="submit" variant="primary" class="bg-green-400">編集</flux:button>
                 </div>
