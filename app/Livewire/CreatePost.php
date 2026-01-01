d6a21629162a902c2635934c567d5de9a3398177
tomoki <tmdesign.0223@gmail.com>
2026-01-02 00:42:30 +0900

    保存処理作成

---
+++ app/Livewire/CreatePost.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\Validate;
+use Livewire\Component;
+
+class CreatePost extends Component
+{
+    #[Validate('required', message: 'タイトルは必須です')]
+    public $title = '';
+
+    #[Validate('required', message: '本文は必須です')]
+    #[Validate('min:10', message: '本文は10文字以上で入力してください')]
+    public $body = '';
+
+    public function save()
+    {
+        $this->validate();
+
+        Post::create([
+            'title' => $this->title,
+            'body' => $this->body,
+            'user_id' => Auth::id()
+        ]);
+
+        session()->flash('status', '投稿できました');
+
+        return $this->redirect('/posts/create');
+    }
+
+    public function render()
+    {
+        return view('livewire.create-post');
+    }
+}
---
+++ resources/views/livewire/create-post.blade.php
@@ -0,0 +1,29 @@
+<div>
+    <flux:heading size="xl" lebel="1">新規投稿ページ</flux:heading>
+
+    <div class="w-3xl m-auto">
+        @if (session('status'))
+            <span class="text-blue-400 bg-white p-1 w-full block rounded-md">{{ session('status') }}</span>
+        @endif
+        {{-- フォーム --}}
+        <form wire:submit="save">
+            <div class="w-3xl mt-4">
+                <input type="text" wire:model="title" placeholder="タイトルを入力してください" class="w-full border-1 rounded-md p-2">
+                @error('title')
+                    <span class="text-red-400">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="w-3xl mt-4">
+                <textarea wire:model="body" placeholder="本文を入力してください" class="w-full border-1 rounded-md p-2"></textarea>
+                @error('body')
+                    <span class="text-red-400">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="flex justify-end mt-4 gap-2">
+                <flux:button href="{{ route('posts') }}" variant="primary" color="blue">一覧に戻る</flux:button>
+                <div>
+                    <flux:button type="submit" variant="primary" class="bg-green-400">投稿</flux:button>
+                </div>
+        </form>
+    </div>
+</div>
