be5bb1a2ae3bf860207c18cf2ca82257414235cf
tomoki <tmdesign.0223@gmail.com>
2026-01-01 23:55:44 +0900

    ルート設定

--- resources/views/livewire/post-index.blade.php
+++ resources/views/livewire/post-index.blade.php
@@ -2,6 +2,6 @@
     <flux:heading size="xl" lebel="1">投稿一覧ページ</flux:heading>
 
     <div class="mt-4">
-        <flux:button href='#' variant="primary" color="blue">新規投稿</flux:button>
+        <flux:button href="{{ route('posts.create') }}" variant="primary" color="blue">新規投稿</flux:button>
     </div>
 </div>
